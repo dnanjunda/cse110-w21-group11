88a8213d83af030c4cacf895450da16d0c6136d9
Ivan Luu <ivluu@ucsd.edu>
2021-02-20 15:39:14 -0800

    Added eventListener for keypresses

--- source/list-btns.js
+++ source/list-btns.js
@@ -1,25 +1,47 @@
+const entName = document.getElementById("task-name");
+entName.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    addTask();
+  }
+});
+
+const entPomo = document.getElementById("pomo-num");
+entPomo.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    addTask();
+  }
+});
+
+/**
+ * Add a new task into the list.
+ */
 function addTask() {
-    const noTask = document.getElementById('no-task');
-    const taskList = document.getElementById('task-list');
-    const taskName = document.getElementById('task-name');
-    const pomoNum = document.getElementById('pomo-num');
+  const noTask = document.getElementById("no-task");
+  const taskList = document.getElementById("task-list");
+  const taskName = document.getElementById("task-name");
+  const pomoNum = document.getElementById("pomo-num");
 
-    noTask.style.display = 'none';
+  noTask.style.display = "none";
 
-    var newTask = taskName.value;
-    var newPomo = pomoNum.value;
-    var task = {
-        'taskName': newTask,
-        'pomoNum': newPomo,
-    }
-    taskList.appendChild(new Task(task));
+  const newTask = taskName.value;
+  const newPomo = pomoNum.value;
+  const task = {
+    taskName: newTask,
+    pomoNum: newPomo,
+  };
+  taskList.appendChild(new Task(task));
 }
 
+/**
+ * Clear the current task list.
+ */
 function clearList() {
-    const taskList = document.getElementById('task-list');
-    const noTask = document.getElementById('no-task');
-    noTask.style.display = 'inline';
-    while (taskList.childNodes.length > 4) {
-        taskList.removeChild(taskList.childNodes[4]);
-    }
+  const taskList = document.getElementById("task-list");
+  const noTask = document.getElementById("no-task");
+  noTask.style.display = "inline";
+  while (taskList.childNodes.length > 4) {
+    taskList.removeChild(taskList.childNodes[4]);
+  }
 }
