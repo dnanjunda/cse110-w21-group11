7663ac48ad6796c993b5be294d6981ed427b120b
James Larsen <jlarsen@ucsd.edu>
2021-02-23 18:47:02 -0800

    Cleaned up settings.js

--- source/scripts/settings.js
+++ source/scripts/settings.js
@@ -14,28 +14,29 @@
 
 */
 
-/* 
- * Timer function
+
+let secondsPerPomo = 60 * 25;           // Number of seconds in single pomo session
+let timeRemaining = secondsPerPomo;     // Time remaining in session in seconds
+let pomodoro = 0;                       // Number of pomodoros completed
+let intervalId = null;                  // ID of interval calling the timeAdvance method
+
+
+
+/**
+ * This function advances time by one second. It will be called by setInterval in startButton
  */
+function timeAdvance() {
+    --timeRemaining;
 
-var pomodoro = 0;
-var twentyfiveMinutes = 60 * 25;
-let minute = 0;
-let seconds = 0;
-let totalSeconds = twentyfiveMinutes;
-let intervalId = null;
-
-function startTimer() {
-    --totalSeconds;
-    seconds = Math.floor(totalSeconds % 60);
-    minute = Math.floor((totalSeconds / 60) % 60);
+    let minute = Math.floor((timeRemaining / 60) % 60);
+    let seconds = Math.floor(timeRemaining % 60);
     minute = minute < 10 ? "0" + minute : minute;
     seconds = seconds < 10 ? "0" + seconds : seconds;
     document.getElementById("minute").innerHTML = minute;
     document.getElementById("seconds").innerHTML = seconds;
-    if(totalSeconds <= 0){ 
+    if(timeRemaining <= 0){ 
         pomodoro++;
-        totalSeconds = twentyfiveMinutes;
+        timeRemaining = secondsPerPomo;
         clearInterval(intervalId);
         document.getElementById("completePomos").innerHTML = "Number of Complete Pomodoros: " + pomodoro;
         sound();
@@ -46,28 +47,29 @@ function startTimer() {
 
     if(pomodoro % pomoBreak == 0 && pomodoro != 0){ //TODO: timer not starting anymore
         alert("Time to take a break");
-        var longBreak = 60 * pomoBreakLength;
-        totalSeconds = longBreak;
+        let longBreak = 60 * pomoBreakLength;
+        timeRemaining = longBreak;
         clearInterval(intervalId);
-        startTimer();
+        timeAdvance();
     }
 }
 
-var resetTimer = document.getElementById("reset-btn");
-resetTimer.addEventListener("click", resetButton);
 
 
-var mixBut = document.getElementById("mixBut");
-mixBut.addEventListener("click", startButton);
+/**
+ * startButton and stopButton will be called by the start/stop button
+ * The single button will swap between the two functions each time one is called
+ */
+const mixBut = document.getElementById("mixBut");
+
 function startButton(){
-    intervalId = setInterval(startTimer, 1000);
+    intervalId = setInterval(timeAdvance, 1000);
     console.log("Started");
     mixBut.removeEventListener("click", startButton);
     mixBut.addEventListener("click", stopButton);
     document.getElementById("mixBut").style.background = "indianred";
     mixBut.value = "Stop";
 }
-
 function stopButton(){
     if (intervalId){
         clearInterval(intervalId);
@@ -78,8 +80,15 @@ function stopButton(){
     document.getElementById("mixBut").style.background = "lightgreen";
     mixBut.value = "Start Timer";
 }
+mixBut.addEventListener("click", startButton);
+
+
+
+/**
+ * resetButton will be linked to the reset button and resets the pomo session
+ */
 function resetButton(){ //TODO: messes up secs if inputted before mins
-    totalSeconds = twentyfiveMinutes;
+    timeRemaining = secondsPerPomo;
     //minutes
     if(inputMins.value == ""){
         document.getElementById("minute").innerHTML = '25';
@@ -105,6 +114,7 @@ function resetButton(){ //TODO: messes up secs if inputted before mins
         document.getElementById("seconds").innerHTML = inputSecs.value;
     }
 }
+document.getElementById("reset-btn").addEventListener("click", resetButton);
 
 
 
@@ -113,41 +123,22 @@ function resetButton(){ //TODO: messes up secs if inputted before mins
  */
 
 
-// Get the modal
-var modal = document.getElementById("settings-modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("settings-btn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
 // When the user clicks anywhere outside of the modal, close it
+const modal = document.getElementById("settings-modal");
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-// var twentyfiveMinutes = 60 * 1;
 
 
-document.getElementById("completePomos").innerHTML = "Number of Complete Pomodoros: " + pomodoro;
-var inputMins = document.getElementById("userMins");
-// TODO: custom seconds(?)
-// var inputSecs = document.getElementById("userSecs");
+/**
+ * Updating timer when the inputMins element in the settings menu changes
+ */
+const inputMins = document.getElementById("userMins");
 inputMins.oninput = function(){
-    Stop(); //so that there's no overlapping timers
+    stopButton(); //so that there's no overlapping timers
     indexMins = 0;
     // doesnt allow for custom timer to start with a 0 and more numbers
     if(inputMins.value.length > 1){
@@ -158,33 +149,34 @@ inputMins.oninput = function(){
     inputMins.value = inputMins.value.substring(indexMins);
     if(inputMins.value == ""){
         document.getElementById("minute").innerHTML = '25';
-        let normal = 25;
-        twentyfiveMinutes = 60 * normal;
+        secondsPerPomo = 60 * 25;
     }
     else if(inputMins.value == "0"){
         document.getElementById("minute").innerHTML = '00';
-        twentyfiveMinutes = 0;
+        secondsPerPomo = inputSecs.value;
     }
     else if(inputMins.value < 10){
         document.getElementById("minute").innerHTML = '0' + inputMins.value;
-        twentyfiveMinutes = 60 * inputMins.value;
+        secondsPerPomo = (60 * inputMins.value) + inputSecs.value;
     }
     else{
         document.getElementById("minute").innerHTML = inputMins.value;
-        twentyfiveMinutes = 60 * inputMins.value;
+        secondsPerPomo = (60 * inputMins.value) + inputSecs.value;
     }
     document.getElementById("seconds").innerHTML = '00';
-    // minute = 0;
-    // seconds = 0;
-    totalSeconds = twentyfiveMinutes;
+    timeRemaining = secondsPerPomo;
     intervalId = null;
 }
 
-// TODO: custom seconds
-var inputSecs = document.getElementById("userSecs");
+
+
+/**
+ * Updating timer when the inputSecs element in the settings menu changes
+ */
+const inputSecs = document.getElementById("userSecs");
 inputSecs.oninput = function(){
-  Stop(); //so that there's no overlapping timers
-  indexSecs = 0;
+  stopButton(); //so that there's no overlapping timers
+  let indexSecs = 0;
   // doesnt allow for custom timer to start with a 0 and more numbers
   if(inputSecs.value.length > 1){
       while(inputSecs.value.substring(indexSecs, indexSecs + 1) == "0"){
@@ -197,35 +189,34 @@ inputSecs.oninput = function(){
   }
   else if(inputSecs.value < 10){
       document.getElementById("seconds").innerHTML = '0' + inputSecs.value;
-      twentyfiveMinutes = (60 * inputMins.value) + inputSecs.value;
+      secondsPerPomo = (60 * inputMins.value) + inputSecs.value;
   }
   else{
       document.getElementById("seconds").innerHTML = inputSecs.value;
-      twentyfiveMinutes = (60 * inputMins.value) + inputSecs.value;
+      secondsPerPomo = (60 * inputMins.value) + inputSecs.value;
   }
-  // document.getElementById("seconds").innerHTML = '00';
-  // // minute = 0;
-  // // seconds = 0;
-  totalSeconds = twentyfiveMinutes;
+  timeRemaining = secondsPerPomo;
   intervalId = null;
 }
+
+
 
 /*
 * Notfication Sound Functions
 */
 function sound(){
-    var x = document.getElementById("changeSelect").value;
-    var volLevel = document.getElementById("volume-slider").value / 100;
+    let x = document.getElementById("changeSelect").value;
+    let volLevel = document.getElementById("volume-slider").value / 100;
     if(x == "Chirp"){
-        var audioSound = new Audio('https://freesound.org/data/previews/456/456440_5121236-lq.mp3');
+        let audioSound = new Audio('https://freesound.org/data/previews/456/456440_5121236-lq.mp3');
         audioSound.volume = volLevel;
     }
     else if(x == "Alarm-Clock"){
-        var audioSound = new Audio('https://freesound.org/data/previews/219/219244_4082826-lq.mp3');
+        let audioSound = new Audio('https://freesound.org/data/previews/219/219244_4082826-lq.mp3');
         audioSound.volume = volLevel;
     }
     else if(x == "None"){
-        var audioSound = new Audio('https://freesound.org/data/previews/219/219244_4082826-lq.mp3');
+        let audioSound = new Audio('https://freesound.org/data/previews/219/219244_4082826-lq.mp3');
         audioSound.volume = 0;
     }
     // infinite loop
@@ -244,55 +235,16 @@ function sound(){
         }
     }
 }
-var slider = document.getElementById("volume-slider");
-var numInp = document.getElementById("volume-number");
+
+
+/**
+ * Making sure the volume-number and the volume-slider always match
+ */
+const slider = document.getElementById("volume-slider");
+const numInp = document.getElementById("volume-number");
 slider.oninput = function(){
     document.getElementById("volume-number").value = document.getElementById("volume-slider").value;
 }  
 numInp.oninput = function(){
     document.getElementById("volume-slider").value = document.getElementById("volume-number").value;
-}
-
-
-
-
-
-/*
-* Task List functions
-*/
-(function(){
-    var todo = document.querySelector( '#tasks' ),
-        form = document.querySelector( 'form' ),
-        field = document.querySelector( '#newitem' );
-    form.addEventListener( 'submit', function( event ) {
-      var text = field.value;
-      if ( text !== '' ) {
-        todo.innerHTML += '<li>' + text +
-          ' <button onclick="Check(this);">check as done</button> <button onclick="Delete(this);">X</button> </li>';
-        field.value = '';
-      }
-      event.preventDefault();
-    }, false);
-  })();
-
-function Check(curr){
-if(curr.parentNode.innerHTML.charAt(0) == "✓"){
-    curr.parentNode.innerHTML= curr.parentNode.innerHTML.substring(1);
-}
-else{
-    curr.parentNode.innerHTML = "✓" + curr.parentNode.innerHTML;
-}
-}
-
-function Delete(curr){
-curr.parentNode.parentNode.removeChild(curr.parentNode);    
-}
-
-var listClear = document.getElementById("clearList");
-
-listClear.addEventListener("click", noList);
-
-function noList(){
-var ul = document.getElementById("tasks");
-ul.innerHTML = "";
 }
