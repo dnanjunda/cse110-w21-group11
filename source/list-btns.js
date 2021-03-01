2fa1948f2c496547fc1afd18c6191e461d64dd83
Ivan Luu <ivluu@ucsd.edu>
2021-02-28 16:39:49 -0800

    Update list-btns.js

--- source/list-btns.js
+++ source/list-btns.js
@@ -26,13 +26,23 @@ function addTask() {
   const noTask = document.getElementById("no-task");
   const tasks = document.getElementById("tasks");
   const taskList = document.getElementById("task-list");
+
   const taskName = document.getElementById("task-name");
   const pomoNum = document.getElementById("pomo-num");
 
   noTask.style.display = "none";
 
-  const newTask = taskName.value;
-  const newPomo = pomoNum.value;
+  
+  let newTask = taskName.value;
+  let newPomo = pomoNum.value;
+
+  if(newTask == null || newTask == "") {
+    newTask = "New Task";
+  }
+  if(newPomo == null || newPomo == "") {
+    newPomo = "1";
+  }
+
   const task = {
     taskName: newTask,
     pomoNum: newPomo,
@@ -49,10 +59,23 @@ clearButton.addEventListener("click",function(){
  * Clear the current task list.
  */
 function clearList() {
-  const taskList = document.getElementById("task-list");
+  const taskList = document.getElementById("tasks");
   const noTask = document.getElementById("no-task");
-  noTask.style.display = "inline";
-  while (taskList.children.length > 2) {
-    taskList.removeChild(taskList.children[2]);
-  }
+  const tasks = document.getElementById("tasks");
+  noTask.style.display = "block";
+  while (taskList.children.length > 1) {
+    taskList.removeChild(taskList.children[1]);
+  } 
+}
+/**
+ * Increment pomos by 1 when plus button is pressed.
+ */
+function increment(){
+  document.getElementById("pomo-num").stepUp();
+
+}
+
+function decrement(){
+  document.getElementById("pomo-num").stepDown();
+
 }
