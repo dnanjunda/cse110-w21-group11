acdceef10175a3245ff1ecd5935411f9ebdcb6b9
Haley Tran <haleytran01@gmail.com>
2021-03-09 16:37:18 -0800

    remove local storage

--- source/scripts/stats.js
+++ source/scripts/stats.js
@@ -2,40 +2,6 @@ var currentTask = null;
 
 sessionStorage.setItem("completed", "[]");
 
-/**
- * load incomplete tasks
- */
-var incomplete = localStorage.getItem("incomplete");
-localStorage.setItem("incomplete", restoreTask());
-
-function restoreTask() {
-    if (incomplete === null || incomplete == "[]") {
-        return "[]";
-    } else {
-        var task = JSON.parse(incomplete);
-        for (var i = 0; i < task.length; i++) {
-            var obj = task[i];
-
-            const noTask = document.getElementById("no-task");
-            const tasks = document.getElementById("tasks");
-            const newTask = document.getElementById("task-name").value;
-            const newPomo = document.getElementById("pomo-num").value;
-
-            noTask.style.display = "none";
-
-            tasks.appendChild(
-                new Task({
-                taskName: obj.task,
-                pomoNum: obj.pomo,
-                })
-            );
-            document.getElementById("form").reset();
-        }
-        return "[]";
-    }
-}
-
-
 
 /**
  * select task or start timer
@@ -80,29 +46,4 @@ function completedTask(el) {
     remove.click();
     document.getElementById("current-task").innerHTML = "Current Task";
     currentTask = null;
-}
-
-/**
- * save incomplete tasks
- * @param {*} event 
- */
-window.onbeforeunload = function(event) {
-    var tasks = document.getElementById("tasks");
-    var taskItem = tasks.children;
-    for (var i = 1; i < taskItem.length; i++) {
-        var shadow = taskItem[i].shadowRoot.childNodes;
-        var form = shadow[1];
-        var name = form.children[0].innerHTML;
-        var pomo = form.children[2].value;
-        console.log(pomo);
-
-        var incomplete = JSON.parse(localStorage.getItem("incomplete"));
-        var task = {
-            "id" : i,
-            "task" : name,
-            "pomo" : pomo
-        };
-        incomplete.push(task);
-        localStorage.setItem("incomplete", JSON.stringify(incomplete));
-    }
 }
